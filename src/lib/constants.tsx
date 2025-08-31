ca3954efa318ec31f1ba97d12c52f49d08a51593
elvisowusu <elvisgyasiowusu24@gmail.com>
2025-08-31 20:26:18 +0000

    done with setup

---
+++ src/lib/constants.tsx
@@ -0,0 +1,35 @@
+import { FooterSection, NavLink } from "@/types/common";
+
+export const navLinks: NavLink[] = [
+  { link: "/", text: "Home" },
+  { link: "/about-us", text: "About Us" },
+  { link: "/services", text: "Services" },
+  { link: "/blog", text: "Blog" },
+  { link: "/projects", text: "Projects" },
+];
+export const footerSections: FooterSection[] = [
+  {
+    title: "Discover",
+    links: [
+      { text: "blog", link: "/blog" },
+      { text: "careers", link: "/careers" },
+    ],
+  },
+  {
+    title: "Legal",
+    links: [
+      { text: "Terms of use", link: "/terms-of-use" },
+      { text: "Privacy Policy", link: "/privacy-policy" },
+      { text: "FAQs", link: "/faq" },
+    ],
+  },
+  {
+    title: "Get Connected",
+    iconLink: [
+      { icon: "/assets/pngs/x.png", link: "https://twitter.com" },
+      { icon: "/assets/pngs/linkedIn.png", link: "https://linkedin.com" },
+      { icon: "/assets/pngs/facebook.png", link: "https://facebook.com" },
+      { icon: "/assets/pngs/instagram.png", link: "https://instagram.com" },
+    ],
+  },
+];
---
+++ src/types/common.ts
@@ -0,0 +1,26 @@
+import { LucideIcon } from "lucide-react";
+
+export interface SidebarTab {
+  title: string;
+  url: string;
+  icon: LucideIcon;
+}
+
+export interface socialIconLink {
+  icon: string;
+  link: string;
+}
+export interface FooterLink {
+  text: string;
+  link: string;
+}
+export interface FooterSection {
+  title: string;
+  links?: FooterLink[];
+  iconLink?: socialIconLink[];
+}
+
+export interface NavLink {
+  link: string;
+  text: string;
+}

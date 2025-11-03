6367fd3d74f11916b7245078a4f6f48320393a0e
elvisowusu <elvisgyasiowusu24@gmail.com>
2025-11-03 15:56:10 +0000

    changes auth layout

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -5,19 +5,9 @@ import SignUpForm from "./components/SignUpForm";
 
 export default function SignUpPage() {
   return (
-    <div className="flex flex-col md:flex-row">
-      {/* Left Side - Image */}
-      <div
-        className=" md:w-1/2 bg-cover bg-center h-[20rem] md:h-screen "
-        style={{
-          backgroundImage: `url(${images.room2.src})`,
-        }}
-      ></div>
-
-      {/* Right Side - Form */}
-      <div className="absolute md:relative rounded-t-2xl md:rounded-none w-full mt-[12rem] md:my-8 md:w-1/2 flex items-center justify-center bg-white">
+    <div>
+     
         <SignUpForm />
-      </div>
     </div>
   );
 }

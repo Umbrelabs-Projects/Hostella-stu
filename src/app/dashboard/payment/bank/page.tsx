08754ddf0da373d2631887acfac69502b333ade4
elvisowusu <elvisgyasiowusu24@gmail.com>
2025-11-10 15:02:49 +0000

    added a reusable payment component and also added momo and bank pages

--- src/app/dashboard/payment/bank/page.tsx
+++ src/app/dashboard/payment/bank/page.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import PaymentMessage from "../reusablecomponent/PaymentMessage";
 
 export default function BankPayment() {
   return (
-    <div>BankPayment</div>
-  )
+    <div className="min-h-screen bg-[#FFF8EC] flex flex-col items-center px-4 py-8">
+      {/* Top Section (Responsive) */}
+      <div className="flex flex-col md:flex-row items-start justify-center gap-8 w-full max-w-5xl">
+        {/* Left: Success Message */}
+        <PaymentMessage />
+
+        {/* Right: Booking Details */}
+        {/* <BookingDetailsCard /> */}
+      </div>
+    </div>
+  );
 }

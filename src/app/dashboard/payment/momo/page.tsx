edfdfac6f17c6a1e2bce07848cc302b6308f54ef
elvisowusu <elvisgyasiowusu24@gmail.com>
2025-12-24 08:10:32 +0000

    feat: update MomoPayment component with suspense loading and improved layout
    - Added "use client" directive to enable client-side rendering in MomoPayment component.
    - Wrapped MomoDetails component in a Suspense fallback with SkeletonCard to enhance loading experience.
    - Adjusted padding in the main container for better visual spacing.

--- src/app/dashboard/payment/momo/page.tsx
+++ src/app/dashboard/payment/momo/page.tsx
@@ -1,17 +1,22 @@
-import React from "react";
+"use client";
+
+import React, { Suspense } from "react";
 import PaymentMessage from "../reusablecomponent/PaymentMessage";
 import MomoDetails from "./components/MomoDetails";
+import { SkeletonCard } from "@/components/ui/skeleton";
 
-export default function BankPayment() {
+export default function MomoPayment() {
   return (
-    <div className="flex flex-col items-center px-4 py-">
+    <div className="flex flex-col items-center px-4 py-8">
       {/* Top Section (Responsive) */}
       <div className="flex flex-col md:flex-row items-start justify-center gap-8 w-full max-w-5xl">
         {/* Left: Success Message */}
         <PaymentMessage />
 
         {/* Right: Momo Details */}
-        <MomoDetails />
+        <Suspense fallback={<SkeletonCard />}>
+          <MomoDetails />
+        </Suspense>
       </div>
     </div>
   );
